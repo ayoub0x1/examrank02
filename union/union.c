6600552abd700e76c471535baf8af6d696d13482
ayoub0x1 <ayoub0x1@wearehackerone.com>
2021-11-24 21:52:42 +0100

    rank02 inchaellah

---
+++ union/union.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   union.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macbook <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/21 20:15:32 by macbook           #+#    #+#             */
+/*   Updated: 2021/11/24 21:46:44 by macbook          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	used[255];
+	int	i, j;
+
+	if (argc == 3)
+	{
+		i = 0;
+		while (i < 255)
+			used[i++] = 0;
+		i = 1;
+		while (i < 3)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if (!used[(unsigned char)argv[i][j]])
+				{
+					used[(unsigned char)argv[i][j]] = 1;
+					write(1, &argv[i][j], 1);
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
